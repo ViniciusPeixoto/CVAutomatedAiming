0129797ca0ee60aba6d88761c45de5d8ffcc7995
ViniciusPeixoto <vinicius.oliveira.peixoto@gmail.com>
2019-04-10 11:22:00 -0300

    This is an attepmt to make a C++ version using openCV's functions but not trying to convert the python algorithm

--- testeOpenCV/testeOpenCV.cpp
+++ testeOpenCV/testeOpenCV.cpp
@@ -1,58 +1,79 @@
-// testeOpenCV.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
+
 using namespace cv;
 using namespace std;
-int main(int, char**)
-{
-	Mat frame;
-	//--- INITIALIZE VIDEOCAPTURE
-	VideoCapture cap;
-	// open the default camera using default API
-	// cap.open(0);
-	// OR advance usage: select any API backend
-	int deviceID = 0;             // 0 = open default camera
-	int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-	// open selected camera using selected API
-	cap.open(deviceID + apiID);
-	// check if we succeeded
+
+int main(){
+
+	//Espaço de memória para a imagem captada da câmera
+	Mat curr_frame;
+	//Espaço para imagens adicionais que serão alteradas
+	Mat color_frame[3];
+
+	//Estrutura de captura de vídeo
+	VideoCapture vcap;
+
+	int deviceID = 0;				//Identificação da câmera
+	int apiID = cv::CAP_ANY;		//Identificação da API
+
+	vcap.set(CAP_PROP_FPS, 30);				//FPS = 30
+	vcap.set(CAP_PROP_FRAME_WIDTH, 320);	//WIDTH = 320px
+	vcap.set(CAP_PROP_FRAME_HEIGHT, 180);	//HIEGHT = 180px
+
+	//Inicialização da câmera
+	vcap.open(deviceID + apiID);
+
+	//Testa se a câmera foi inicializada
 	if (!cap.isOpened()) {
 		cerr << "ERROR! Unable to open camera\n";
 		return -1;
 	}
-	//--- GRAB AND WRITE LOOP
-	cout << "Start grabbing" << endl
-		<< "Press any key to terminate" << endl;
-	for (;;)
-	{
-		// wait for a new frame from camera and store it into 'frame'
-		cap.read(frame);
-		// check if we succeeded
-		if (frame.empty()) {
+
+	//int new_h;
+	//int new_w;
+
+	Mat paint[3] = Mat::zeros(CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CV_8UC1);
+	Mat imagem_comp = Mat::zeros(CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CV_8UC1);
+
+
+	//Canal de cor a ser usado como pincel
+	int color_of_paint = 0;
+
+	//valor do controle de luminosidade do canal de cor
+	int light_control = 240;
+
+	//Tempo que o texto será exibido
+	int timer = 15;
+
+	//Texto que será mostrado ao alterar a configuração de luminosidade
+	String text = "";
+
+	while(true){
+
+		//Captura a imagem da câmera
+		vcap.read(curr_frame);
+
+		//Verifica se foi bem sucedida a captura da imagem
+		if (curr_frame.empty()) {
 			cerr << "ERROR! blank frame grabbed\n";
 			break;
 		}
-		// show live and wait for a key with timeout long enough to show images
-		imshow("Live", frame);
-		if (waitKey(5) >= 0)
-			break;
+		
+		//Transfere o canal de cor definido em color_of_paint para o color_frame
+		cv::split(curr_frame, color_frame);
+
+		cv::compare(color_frame[color_of_paint], light_control, paint[color_of_paint], CMP_GT);
+		cv::compare(color_frame[color_of_paint], light_control+20, imagem_comp, CMP_LT);
+		cv::compare(imagem_comp, paint[color_of_paint], paint[color_of_paint], CMP_EQ);
+
+		cv::bitwise_or(color_frame[color_of_paint], paint[color_of_paint]);
+
+
 	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
+
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
