f364202c2e3a82e062c86e7e4eb197f55b09410d
ViniciusPeixoto <vinicius.oliveira.peixoto@gmail.com>
2019-04-10 10:38:48 -0300

    Adding test files for openCV project
    These are the first files for the test project.
    The test project consists in replicate the fourth project in the Image Processing class, 2018-2 semester.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CVAutomatedAiming
 This is a repository for the files of a Computer Vision Automatic Aiming System. This is a Scientific Initiation project.
+
+#Tools
+This project is being developed using:
+
+- Microsoft Visual Studio 2019
+- OpenCV 4.0.1 Library
+
+The correct configuration of these tools is needed for the project to run.
---
+++ testeOpenCV/testeOpenCV.cpp
@@ -0,0 +1,58 @@
+// testeOpenCV.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
+using namespace cv;
+using namespace std;
+int main(int, char**)
+{
+	Mat frame;
+	//--- INITIALIZE VIDEOCAPTURE
+	VideoCapture cap;
+	// open the default camera using default API
+	// cap.open(0);
+	// OR advance usage: select any API backend
+	int deviceID = 0;             // 0 = open default camera
+	int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+	// open selected camera using selected API
+	cap.open(deviceID + apiID);
+	// check if we succeeded
+	if (!cap.isOpened()) {
+		cerr << "ERROR! Unable to open camera\n";
+		return -1;
+	}
+	//--- GRAB AND WRITE LOOP
+	cout << "Start grabbing" << endl
+		<< "Press any key to terminate" << endl;
+	for (;;)
+	{
+		// wait for a new frame from camera and store it into 'frame'
+		cap.read(frame);
+		// check if we succeeded
+		if (frame.empty()) {
+			cerr << "ERROR! blank frame grabbed\n";
+			break;
+		}
+		// show live and wait for a key with timeout long enough to show images
+		imshow("Live", frame);
+		if (waitKey(5) >= 0)
+			break;
+	}
+	// the camera will be deinitialized automatically in VideoCapture destructor
+	return 0;
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
